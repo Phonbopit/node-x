7bf41541d05891e58e4b896c733b65981a7af1ff
Chai Phonbopit <phonbopit@gmail.com>
2014-12-07 21:07:26 +0700

    init

---
+++ app.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function(req, res) {
+	res.send('<h1>Hello Express.js</h1>');
+});
+
+
+
+
+
+var server = app.listen(3000, function() {
+	console.log('Listening on port 3000');
+});
---
+++ without-express.js
@@ -0,0 +1,10 @@
+var http = require('http');
+var server = http.createServer(function(req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.write("<h1>Hello World</h1>");
+  res.end();
+});
+
+server.listen(3000, function() {
+  console.log("Go to localhost:3000 on your browser");
+});
