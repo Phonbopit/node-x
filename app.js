5c172112b551196d777187613bc1af1b43df217a
Chai Phonbopit <phonbopit@gmail.com>
2014-12-07 23:06:09 +0700

    test passed

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var app = express();
 app.set('view engine', 'ejs');
 //app.set('views', __dirname + '/views');
 
+
 app.get('/', function(req, res) {
 	console.log(__dirname);
 	res.render('default', {
--- test/test.js
+++ test/test.js
@@ -4,20 +4,46 @@ var expect = require('chai').expect;
 
 describe('GET "/"', function() {
 
-  it('page exist', function(done) {
+  it('route / exist', function(done) {
     request(app)
       .get('/')
+      .expect('Content-Type', /html/)
       .expect(200, done);
   });
 
-  it('response content', function(done) {
+  it('response / content', function(done) {
     request(app)
       .get('/')
       .expect(200)
       .end(function(err, res) {
         if (err) return done(err);
-        expect(body).to.contain('Node.js + Express.js');
+        expect(res.text).to.contain('Node.js + Express.js');
         done();
+      });
+  });
+
+});
+
+describe('GET "/me"', function() {
+
+  it('route /me exist', function(done) {
+    request(app)
+      .get('/')
+      .expect('Content-Type', /html/)
+      .expect(200, done);
+  });
+
+  it('response "/me" content', function(done) {
+    request(app)
+      .get('/me')
+      .expect(200)
+      .end(function(err, res) {
+        if (err) {
+          return done(err);
+        } else {
+          expect(res.text).to.contain('Chai');
+          done();
+        }
       });
   });
 
