fe1e40852efed2fc8ac646a8083962a454531aae
Chai Phonbopit <phonbopit@gmail.com>
2014-12-07 21:18:31 +0700

    add ejs to template engine

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
 var express = require('express');
 var app = express();
 
+app.set('view engine', 'ejs');
+
 app.get('/', function(req, res) {
 	res.send('<h1>Hello Express.js</h1>');
 });
 
+app.get('/me', function(req, res) {
+	res.send('Chai');
+});
 
+app.get('/who/:name', function(req, res) {
+	var name = req.params.name;
+	res.send('<h1>Hello xxxxx</h1> ' + name);
+});
 
-
+app.get('*', function(req, res) {
+	res.send("ASFSDFSDFDF");
+});
 
 var server = app.listen(3000, function() {
 	console.log('Listening on port 3000');
